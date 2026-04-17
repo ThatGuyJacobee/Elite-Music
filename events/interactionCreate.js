4a91a238c0a83a123e4ad16733a999eb0187b71c
Juli04 <julian97@markusalker.de>
2026-04-17 20:27:12 +0200

    Fix typo in volume adjust modal

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -485,7 +485,7 @@ module.exports = {
                 //
                 const modal = new ModalBuilder()
                 .setCustomId(`adjust_volume_${interaction.guild.id}`)
-                .setTitle(`Adjsut Volume - Currently at ${queue.node.volume}%`)
+                .setTitle(`Adjust Volume - Currently at ${queue.node.volume}%`)
                 .addComponents([
                     new ActionRowBuilder().addComponents(
                     new TextInputBuilder()
